124006c915b91a2a8ee49ecbfdc27657291de411
Evan Shabsove <evan.shabsove@gmail.com>
2017-03-22 19:34:11 -0400

    added dispatcher, now going to add a way to send actions to the dispatcher

--- flux/src/js/stores/TodoStore.js
+++ flux/src/js/stores/TodoStore.js
@@ -1,5 +1,7 @@
 import { EventEmitter } from "events";
 
+import dispatcher from "../dispatcher";
+
 class TodoStore extends EventEmitter {
   constructor(){
     super()
@@ -33,8 +35,18 @@ class TodoStore extends EventEmitter {
     return this.todos;
   }
 
+  handleActions(action) {
+    switch(action.type) {
+      case "CREATE_TODO": {
+        this.createTodo(action.text);
+      }
+    }
+  }
+
 
 }
 
 const todoStore = new TodoStore;
+dispatcher.register(todoStore.handleActions.bind(todoStore))
+window.dispatcher = dispatcher;
 export default todoStore
