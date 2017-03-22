ab805bee93d594a89ab68262e0bc451fd6ec4af6
Evan Shabsove <evan.shabsove@gmail.com>
2017-03-22 18:37:22 -0400

    can now update the components if a change happens to the store, adding a flux dispatcher now

--- flux/src/js/pages/Todos.js
+++ flux/src/js/pages/Todos.js
@@ -8,24 +8,18 @@ import TodoStore from "../stores/TodoStore";
 export default class Todos extends React.Component {
   constructor() {
     super();
-    // this.state = {
-    //   todos: [
-    //     {
-    //       id: 113464613,
-    //       text: "Go Shopping",
-    //       complete: false
-    //     },
-    //     {
-    //       id: 235684679,
-    //       text: "Pay Bills",
-    //       complete: false
-    //     },
-    //   ],
-    // };
 
     this.state = {
       todos: TodoStore.getAll(),
     };
+  }
+
+  componentWillMount() {
+    TodoStore.on("change", () => {
+      this.setState({
+        todos: TodoStore.getAll(),
+      });
+    });
   }
 
   render() {
--- flux/src/js/stores/TodoStore.js
+++ flux/src/js/stores/TodoStore.js
@@ -4,23 +4,37 @@ class TodoStore extends EventEmitter {
   constructor(){
     super()
     this.todos = [
-        {
-          id: 113464613,
-          text: "Go Shopping",
-          complete: false
-        },
-        {
-          id: 235684679,
-          text: "Pay Water",
-          complete: false
-        },
-      ];
-    }
+      {
+        id: 113464613,
+        text: "Go Shopping",
+        complete: false
+      },
+      {
+        id: 235684679,
+        text: "Pay Water",
+        complete: false
+      },
+    ];
+  }
+
+  createTodo(text) {
+    const id = Date.now
+
+    this.todos.push({
+      id,
+      text,
+      complete: false,
+    })
+
+    this.emit("change")
+  }
+
   getAll() {
     return this.todos;
   }
+
+
 }
 
 const todoStore = new TodoStore;
-
 export default todoStore
